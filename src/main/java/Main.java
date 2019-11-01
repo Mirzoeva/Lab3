e2aef6a0b40423e6483ae775ffbb27a0bf22daf4
Mirzoeva <vselennaya314@gmail.com>
2019-11-01 21:09:56 +0300

    Scripted auto-commit on change (2019-11-01 21:09:56) by gitwatch.sh

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,9 +12,8 @@ public class Main {
         SparkConf conf = new SparkConf().setAppName("lab3");
         JavaSparkContext sc = new JavaSparkContext(conf);
         JavaRDD<String> flightsLines = sc.textFile("664600583_T_ONTIME_sample.csv");
-        FlightData flightData = new FlightData()
         JavaRDD<String[]> flightsLinesParsed = flightsLines
-                .map(new FlightData flight = FlightData)
+                .map(ParserUtils::splitAll)
                 .filter(cols -> isNotColumnName(cols, FLIGHT_DEST_AIRPORT_INDEX, FLIGHT_DEST_AIRPORT_COLUMN_NAME));
 
     }
