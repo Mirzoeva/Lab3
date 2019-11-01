c598a1f6914c8ba696b747ebc4322e159cb1b008
Mirzoeva <vselennaya314@gmail.com>
2019-11-01 21:26:35 +0300

    Scripted auto-commit on change (2019-11-01 21:26:35) by gitwatch.sh

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,6 +5,7 @@ import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
+import scala.Tuple2;
 
 
 public class Main {
