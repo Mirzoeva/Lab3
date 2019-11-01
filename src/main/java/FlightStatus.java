8ad1927137866d318222ae7da26563c95d84c1e4
Mirzoeva <vselennaya314@gmail.com>
2019-11-01 22:03:58 +0300

    Scripted auto-commit on change (2019-11-01 22:03:58) by gitwatch.sh

--- src/main/java/FlightStatus.java
+++ src/main/java/FlightStatus.java
@@ -2,8 +2,13 @@
 import java.io.Serializable;
 
 public class FlightStatus implements Serializable{
-
+    private float maxDelay;
+    private int delayedFlights;
+    private int cancelledFlights;
+    private int flights;
     
+
+
 
 }
 
