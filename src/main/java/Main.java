675fc81b81c3de8355f4e3a1d0283dac1d10d4ef
Mirzoeva <vselennaya314@gmail.com>
2019-11-01 19:59:39 +0300

    Scripted auto-commit on change (2019-11-01 19:59:39) by gitwatch.sh

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,6 +4,8 @@ import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaSparkContext;
 
 
 public class Main {
@@ -13,19 +15,10 @@ public class Main {
 
 
 
-        //"Hello World! QQQQQQQQ"
         Job job = Job.getInstance();
         job.setJarByClass(Main.class);
         job.setJobName("Main sort");
-//        MultipleInputs.addInputPath(job, new Path(args[0]), TextInputFormat.class, MapperAirports.class);
-//        MultipleInputs.addInputPath(job, new Path(args[1]), TextInputFormat.class, MapperFlights.class);
         FileOutputFormat.setOutputPath(job, new Path(args[2]));
-//        job.setPartitionerClass(PartitionerAirports.class);
-//        job.setGroupingComparatorClass(ComparatorAirports.class);
-//        job.setReducerClass(JoinReducer.class);
-//        job.setMapOutputKeyClass(TextPair.class);
-//        job.setOutputKeyClass(Text.class);
-//        job.setOutputValueClass(Text.class);
         job.setNumReduceTasks(2);
         System.exit(job.waitForCompletion(true) ? 0 : 1);
     }
