f4bf42c47789152c5bd0aad9064eb5a8ca15669c
Mirzoeva <vselennaya314@gmail.com>
2019-11-01 21:11:00 +0300

    Scripted auto-commit on change (2019-11-01 21:11:00) by gitwatch.sh

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,13 +8,20 @@ import org.apache.spark.api.java.JavaRDD;
 
 
 public class Main {
+    private static final int ID_ROW = 14;
+    private static final int DELAY_ROW = 18;
+
+    private static boolean isNotColumnName(String[] cols, int columnIndex, String columnName) {
+        return !cols[columnIndex].equals(columnName);
+    }
+
     public static void main(String[] args) throws Exception {
         SparkConf conf = new SparkConf().setAppName("lab3");
         JavaSparkContext sc = new JavaSparkContext(conf);
         JavaRDD<String> flightsLines = sc.textFile("664600583_T_ONTIME_sample.csv");
         JavaRDD<String[]> flightsLinesParsed = flightsLines
                 .map(ParserUtils::splitAll)
-                .filter(cols -> isNotColumnName(cols, FLIGHT_DEST_AIRPORT_INDEX, FLIGHT_DEST_AIRPORT_COLUMN_NAME));
+                .filter(cols -> isNotColumnName(cols, ID_ROW, FLIGHT_DEST_AIRPORT_COLUMN_NAME));
 
     }
 }
