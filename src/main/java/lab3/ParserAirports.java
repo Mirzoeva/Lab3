04b2a76154b34fdc3f87494ac4a34b96af780bc6
Mirzoeva <vselennaya314@gmail.com>
2019-11-09 05:28:24 +0300

    Scripted auto-commit on change (2019-11-09 05:28:24) by gitwatch.sh

--- src/main/java/lab3/ParserAirports.java
+++ src/main/java/lab3/ParserAirports.java
@@ -1,4 +1,26 @@
 package lab3;
 
+import org.apache.hadoop.io.Text;
+
 public class ParserAirports {
+    private String[] mainString;
+    private static final int ID_ROW = 0;
+    private static final int NAME_ROW = 1;
+
+    public ParserAirports(String str){
+        mainString = ParserUtils.splitAll(str);
+    }
+
+    public ParserAirports(Text str){
+        mainString = ParserUtils.splitAll(str.toString());
+    }
+
+    public String getAirportID(){
+        return mainString[ID_ROW];
+    }
+
+    public String getValue(){
+        return mainString[NAME_ROW];
+    }
+
 }
