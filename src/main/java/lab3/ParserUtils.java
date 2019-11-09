a0701c19b0b9162efe1ca695371c89832c8ad75a
Mirzoeva <vselennaya314@gmail.com>
2019-11-09 03:51:48 +0300

    Scripted auto-commit on change (2019-11-09 03:51:48) by gitwatch.sh

--- src/main/java/lab3/ParserUtils.java
+++ src/main/java/lab3/ParserUtils.java
@@ -1,7 +1,7 @@
 package lab3;
 public class ParserUtils {
     public static String[] splitAll(String str){
-        String[] split = str.split(",");
+        String[] split = str.split(",[\"]");
         for (int i = 0; i < split.length; i++){
             split[i] = split[i].replaceAll("\"", "");
         }
