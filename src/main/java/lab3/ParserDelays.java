508afe73ef8e8d625b5cc39d641217e3a44833be
Mirzoeva <vselennaya314@gmail.com>
2019-11-09 05:29:10 +0300

    Scripted auto-commit on change (2019-11-09 05:29:10) by gitwatch.sh

--- src/main/java/lab3/ParserDelays.java
+++ src/main/java/lab3/ParserDelays.java
@@ -1,4 +1,26 @@
 package lab3;
 
+import org.apache.hadoop.io.Text;
+
 public class ParserDelays {
+    private String[] mainString;
+    private static final int ID_ROW = 14;
+    private static final int DELAY_ROW = 18;
+
+    public ParserDelays(String str){
+        mainString = ParserUtils.splitAll(str);
+    }
+
+    public ParserDelays(Text str){
+        mainString = ParserUtils.splitAll(str.toString());
+    }
+
+    public String getAirportID(){
+        return mainString[ID_ROW];
+    }
+
+    public String getValue(){
+        return mainString[DELAY_ROW];
+    }
+
 }
